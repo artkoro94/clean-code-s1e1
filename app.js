c1cb82aee13305b3f954b5e185458dcf1594da0e
artkoro94 <artkoro94s@gmail.com>
2025-10-19 19:23:28 +0400

    style: complete 2.1 formatting in 1.1 html-and-css.md

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ addButton.addEventListener("click",addTask);
 addButton.addEventListener("click",ajaxRequest);
 
 
-var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
+var bindTaskEvents=function(taskListItem,checkBoxEventHandler) {
   console.log("bind list item events");
   //select ListItems children
   var checkBox=taskListItem.querySelector("input[type=checkbox]");
